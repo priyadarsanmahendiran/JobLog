96d5ff73d10d44bffa2ced97d94fc38b294322c9
Priyadarsan Mahendiran <m.priyan2001@gmail.com>
2025-04-20 20:18:02 +0530

    Add jacoco plugin

--- backend/src/main/java/com/joblog/exceptions/UserNotFoundException.java
+++ backend/src/main/java/com/joblog/exceptions/UserNotFoundException.java
@@ -3,15 +3,15 @@ package com.joblog.exceptions;
 import java.util.UUID;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
+  public UserNotFoundException(String message) {
+    super(message);
+  }
 
-    public UserNotFoundException(UUID userId) {
-        super("User with ID " + userId + " not found.");
-    }
+  public UserNotFoundException(UUID userId) {
+    super("User with ID " + userId + " not found.");
+  }
 
-    public UserNotFoundException(String field, String value) {
-        super("User with " + field + " '" + value + "' not found.");
-    }
+  public UserNotFoundException(String field, String value) {
+    super("User with " + field + " '" + value + "' not found.");
+  }
 }
